34414f817c69dfeb3224007526bbcf2f7ec18bbb
Andrea Patrizio <andrea.patrizio@gmail.com>
2012-05-05 12:57:21 +0200

    homework2

--- 2012-05-04/exercise4.js
+++ 2012-05-04/exercise4.js
@@ -1,46 +1,63 @@
 var Wing = function() {
 	var domain1 = INTERVALS(1)(30);
 
-	var controls0 = [[1.25,0,0],[0,0.3,0],[-1.25,0,0],[0,-0.3,0],[1.25,0,0]];
-	var c0 = BEZIER(S0)(controls0);
-	var curve0 = MAP(c0)(domain1);
+var controls0 = [[-0.4,4,0],[-0.4,2.25,1.5],[-0.4,-6,0],[-0.4,2.25,-1.5],[-0.4,4,0]];
+var c0 = BEZIER(S0)(controls0);
+var curve0 = MAP(c0)(domain1);
 
-	var controls1 = [[1.25,0,1],[0,0.3,1],[-1.25,0,1],[0,-0.3,1],[1.25,0,1]];
-	var c1 = BEZIER(S0)(controls1);
-	var curve1 = MAP(c1)(domain1);
+var controls1 = [[0,2.7,0],[0,1.35,0.7],[0,-3,0],[0,1.3,-0.7],[0,2.7,0]];
+var c1 = BEZIER(S0)(controls1);
+var curve1 = MAP(c1)(domain1);
 
-	var controls2 = [[1,0.1,2],[0,0.4,2],[-1,0.1,2],[0,-0.2,2],[1,0.1,2]];
-	var c2 = BEZIER(S0)(controls2);
-	var curve2 = MAP(c2)(domain1);
+var controls2 = [[0.5,2.1,0],[0.5,1.35,0.5],[0.5,-2.3,0],[0.5,1.3,-0.5],[0.5,2.1,0]];
+var c2 = BEZIER(S0)(controls2);
+var curve2 = MAP(c2)(domain1);
 
-	var controls3 = [[1,0.2,3],[0,0.5,3],[-1.25,0.2,3],[0,-0.1,3],[1,0.2,3]];
-	var c3 = BEZIER(S0)(controls3);
-	var curve3 = MAP(c3)(domain1);
+var controls3 = [[1,2.1,0],[1,1.35,0.5],[1,-2.3,0],[1,1.3,-0.5],[1,2.1,0]];
+var c3 = BEZIER(S0)(controls3);
+var curve3 = MAP(c3)(domain1);
 
-	var controls4 = [[0.7,0.3,4],[0,0.6,4],[-1.25,0.3,4],[0,0,4],[0.7,0.3,4]];
-	var c4 = BEZIER(S0)(controls4);
-	var curve4 = MAP(c4)(domain1);
+var controls4 = [[1.5,2.05,0.045],[1.5,1,0.5],[1.5,-1.5,.045],[1.5,1,-0.4],[1.5,2.05,0.045]];
+var c4 = BEZIER(S0)(controls4);
+var curve4 = MAP(c4)(domain1);
 
-	var controls5 = [[0.5,0.4,4.5],[0,0.7,4.5],[-1.25,0.4,4.5],[0,0.1,4.5],[0.5,0.4,4.5]];
-	var c5 = BEZIER(S0)(controls5);
-	var curve5 = MAP(c5)(domain1);
+var controls5 = [[2,2,0.115],[2,1,0.6],[2,-1.4,0.115],[2,1,-0.3],[2,2,0.115]];
+var c5 = BEZIER(S0)(controls5);
+var curve5 = MAP(c5)(domain1);
 
-	var controls6 = [[0.3,0.5,4.6],[0,0.8,4.6],[-0.6,0.5,4.6],[0,0.2,4.6],[0.3,0.5,4.6]];
-	var c6 = BEZIER(S0)(controls6);
-	var curve6 = MAP(c6)(domain1);
+var controls6 = [[2.5,1.95,0.16],[2.5,1,0.6],[2.5,-1.2,0.16],[2.5,1,-0.25],[2.5,1.95,0.16]];
+var c6 = BEZIER(S0)(controls6);
+var curve6 = MAP(c6)(domain1);
 
-	var controls7 = [[0.15,0.6,4.75],[0,0.9,4.75],[-0.3,0.6,4.75],[0,0.3,4.75],[0.15,0.6,4.75]];
-	var c7 = BEZIER(S0)(controls7);
-	var curve7 = MAP(c7)(domain1);
+var controls7 = [[3,1.9,0.205],[3,1,0.5],[3,-1.1,0.205],[3,1,-0.1],[3,1.9,0.205]];
+var c7 = BEZIER(S0)(controls7);
+var curve7 = MAP(c7)(domain1);
 
-	var controls8 = [[0,0.6,4.75]];
-	var c8 = BEZIER(S0)(controls8);
-	var curve8 = MAP(c8)(domain1);
+var controls8 = [[3.5,1.85,0.25],[3.5,1,0.5],[3.5,-0.9,0.25],[3.5,1,0],[3.5,1.85,0.25]];
+var c8 = BEZIER(S0)(controls8);
+var curve8 = MAP(c8)(domain1);
 
-	var domain2 = DOMAIN([[0,1],[0,1]])([30,50]);
-	var s = BEZIER(S1)([c0,c1,c2,c3,c4,c5,c6,c7,c8]);
-	var surf = MAP(s)(domain2);
-	return surf;
+var controls9 = [[4,1.8,0.295],[4,1,0.5],[4,-0.75,0.295],[4,1,0.1],[4,1.8,0.295]];
+var c9 = BEZIER(S0)(controls9);
+var curve9 = MAP(c9)(domain1);
+
+var controls10 = [[4.5,1.7,0.34],[4.5,1,0.5],[4.5,-0.55,0.34],[4.5,1,0.2],[4.5,1.7,0.34]];
+var c10 = BEZIER(S0)(controls10);
+var curve10 = MAP(c10)(domain1);
+
+var controls11 = [[5,1.5,0.41],[5,1,0.55],[5,-0.25,0.41],[5,1,0.3],[5,1.5,0.41]];
+var c11 = BEZIER(S0)(controls11);
+var curve11 = MAP(c11)(domain1);
+
+var controls12 = [[5,1,0.41]];
+var c12 = BEZIER(S0)(controls12);
+var curve12 = MAP(c12)(domain1);
+
+var domain2 = DOMAIN([[0,1],[0,1]])([40,80]);
+var s = BEZIER(S1)([c0,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12]);
+var surf = MAP(s)(domain2);
+return surf;
+
 }
 
 var Fuselage = function() {
@@ -184,7 +201,7 @@ var curve16 = MAP(c16)(domain1);
 	return STRUCT([vstab,hstabs]);
 }
 
-var w1 = T([0,1])([1.3,-0.6])(Wing());
+var w1 = T([0,1,2])([1.5,-0.25,0.4])(R([0,1])(-PI/2)(R([0,2])([-PI/2])(Wing())));
 var w2 = S([2])([-1])(w1);
 
 var f = Fuselage();
